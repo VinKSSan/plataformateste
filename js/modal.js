3162b77a6aa509259c6690effcb09048307c87c1
VinKSSan <vinsan.ed@proton.me>
2023-04-21 15:50:09 +0000

    adicionado o modal da opção 'Quem somos' do menu de navegação, codigo css em modo produção

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+const dropdown = document.querySelector('.dropdown')
+const btn_drop = document.querySelector('.btn_quem')
+const cabeça = document.querySelector('.cabeça')
+
+
+function abrir_drop(event) {
+    event.preventDefault()
+    //event.stopPropagation()
+    dropdown.classList.toggle('active')
+}
+function fechar_drop(event) {
+    dropdown.classList.remove('active')
+}
+btn_drop.addEventListener('click' , abrir_drop)
+cabeça.addEventListener('mouseleave' , fechar_drop)
